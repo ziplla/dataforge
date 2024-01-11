739fd2e5309ce8be3d56b083fd6282a246d13a30
Pavel <pzajtsev@yandex.ru>
2024-01-11 23:24:29 +0400

    create generation methods for int and string

---
+++ src/main/java/com/zipla/dataforge/DataGenerator.java
@@ -0,0 +1,76 @@
+package com.zipla.dataforge;
+
+import lombok.ToString;
+
+import java.util.HashMap;
+
+import java.util.Map;
+import java.util.Random;
+
+@ToString
+public class DataGenerator {
+
+    private final Map<String, Object> generatedData;
+    private final Map<String, StringConstraint> stringConstraints;
+    private final Map<String, IntConstraint> intConstraints;
+
+    public DataGenerator() {
+        this.generatedData = new HashMap<>();
+        this.stringConstraints = new HashMap<>();
+        this.intConstraints = new HashMap<>();
+    }
+
+    public void addStringField(String fieldName, StringConstraint constraints) {
+        stringConstraints.put(fieldName, constraints);
+    }
+
+    public void addIntegerField(String fieldName, IntConstraint constraints) {
+        intConstraints.put(fieldName, constraints);
+    }
+
+    public Map<String, Object> generate() {
+        for (String fieldName : stringConstraints.keySet()) {
+            generatedData.put(fieldName, generateString(fieldName));
+        }
+
+        for (String fieldName : intConstraints.keySet()) {
+            generatedData.put(fieldName, generateInt(fieldName));
+        }
+
+        return generatedData;
+    }
+
+    private String generateString(String fieldName) {
+        StringConstraint constraint = stringConstraints.get(fieldName);
+        int minLength = constraint.getMinLength();
+        int maxLength = constraint.getMaxLength();
+
+        // Логика генерации строки в пределах заданных ограничений
+        return generateRandomString(minLength, maxLength);  // Замените на вашу реальную логику генерации
+    }
+    public static String generateRandomString(int minLength, int maxLength) {
+        if (minLength < 0 || maxLength < 0 || minLength > maxLength) {
+            throw new IllegalArgumentException("Некорректные параметры длины строки");
+        }
+
+        Random random = new Random();
+        int length = minLength + random.nextInt(maxLength - minLength + 1);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            char randomChar = (char) (random.nextInt(26) + 'a'); // Генерация случайной буквы от 'a' до 'z'
+            sb.append(randomChar);
+        }
+
+        return sb.toString();
+    }
+
+    private int generateInt(String fieldName) {
+        IntConstraint constraint = intConstraints.get(fieldName);
+        int min = constraint.getMin();
+        int max = constraint.getMax();
+
+        // Логика генерации числа в пределах заданных ограничений
+        return min + (int)(Math.random() * max);  // Замените на вашу реальную логику генерации
+    }
+}

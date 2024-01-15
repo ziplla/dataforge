07fb0e0a0a76ffcd7c399ab0a9fad79f5495aa44
Pavel <pzajtsev@yandex.ru>
2024-01-16 00:43:44 +0400

    create test for NameTemplate

---
+++ src/test/java/ru/ziplla/dataforge/templates_tests/NameTemplateTest.java
@@ -0,0 +1,28 @@
+package ru.ziplla.dataforge.templates_tests;
+
+import org.junit.Test;
+import ru.ziplla.dataforge.templates.NameTemplate;
+
+import static org.junit.Assert.*;
+
+public class NameTemplateTest {
+
+    @Test
+    public void generateRandomNameShouldNotReturnNull() {
+        String randomName = NameTemplate.generateRandomName();
+        assertNotNull("Generated name should not be null", randomName);
+    }
+
+    @Test
+    public void generateRandomNameShouldNotThrowException() {
+        try {
+            String randomName = NameTemplate.generateRandomName();
+            assertNotNull("Generated name should not be null", randomName);
+        } catch (Exception e) {
+            fail("Generate random name should not throw an exception");
+        }
+    }
+
+}
+
+

89c910042350b54345580b804b7cf307b29ad3bf
Pavel <pzajtsev@yandex.ru>
2024-01-20 22:46:10 +0400

    transferring test data to email.yml

--- src/main/java/ru/ziplla/dataforge/templates/EmailTemplate.java
+++ src/main/java/ru/ziplla/dataforge/templates/EmailTemplate.java
@@ -1,5 +1,11 @@
 package ru.ziplla.dataforge.templates;
 
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class EmailTemplate implements Template{
@@ -15,11 +21,25 @@ public class EmailTemplate implements Template{
     public static String generateRandomEmail() {
         Random random = new Random();
 
-        String prefix = PREFIXES[random.nextInt(PREFIXES.length)];
-        String suffix = SUFFIXES[random.nextInt(SUFFIXES.length)];
-        String provider = EMAIL_PROVIDERS[random.nextInt(EMAIL_PROVIDERS.length)];
+        Yaml yaml = new Yaml();
 
+        try (InputStream inputStream = EmailTemplate.class.getClassLoader().getResourceAsStream("email.yml")) {
 
-        return prefix + suffix + "@" + provider + ".com";
+            Map<String, List<String>> data = yaml.load(inputStream);
+
+            List<String> providers = data.get("providers");
+            List<String> prefixes = data.get("prefixes");
+            List<String> suffixes = data.get("suffixes");
+
+            String provider = providers.get(random.nextInt(providers.size()));
+            String prefix = prefixes.get(random.nextInt(prefixes.size()));
+            String suffix = suffixes.get(random.nextInt(suffixes.size()));
+
+            return prefix + suffix + "@" + provider + ".com";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Exception";
+        }
+
     }
 }

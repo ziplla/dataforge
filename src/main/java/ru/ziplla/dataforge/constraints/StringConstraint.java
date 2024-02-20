ef17d8e190a7a4b4bc97c81dfa9a748be08d2279
Pavel <pzajtsev@yandex.ru>
2024-02-20 13:39:56 +0400

    create param with language for StringConstraint

--- src/main/java/ru/ziplla/dataforge/constraints/StringConstraint.java
+++ src/main/java/ru/ziplla/dataforge/constraints/StringConstraint.java
@@ -1,13 +1,24 @@
 package ru.ziplla.dataforge.constraints;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@AllArgsConstructor
+
 @Getter
 @Setter
 public class StringConstraint {
     private final int firstLimit;
     private final int secondLimit;
+    private Language language;
+
+    public StringConstraint(int firstLimit, int secondLimit, Language language) {
+        this.firstLimit = firstLimit;
+        this.secondLimit = secondLimit;
+        this.language = language;
+    }
+
+    public StringConstraint(int firstLimit, int secondLimit) {
+        this.firstLimit = firstLimit;
+        this.secondLimit = secondLimit;
+    }
 }
